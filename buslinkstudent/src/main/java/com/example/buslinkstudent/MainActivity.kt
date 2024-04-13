f1e9ae5822a5297cd93fa4b27f94392c8d3cc584
CherifiMi <cherifi154949@gmail.com>
2024-04-13 03:20:44 +0100

    .

--- buslinkstudent/src/main/java/com/example/buslinkstudent/MainActivity.kt
+++ buslinkstudent/src/main/java/com/example/buslinkstudent/MainActivity.kt
@@ -3,25 +3,39 @@ package com.example.buslinkstudent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.buslinkstudent.theme.BusLinkStudentTheme
-import com.example.common.util.readFromWebSocket
 import com.mapbox.common.MapboxOptions
 import com.mapbox.geojson.Point
 import com.mapbox.geojson.Polygon
@@ -31,7 +45,6 @@ import com.mapbox.maps.extension.compose.DefaultSettingsProvider
 import com.mapbox.maps.extension.compose.DefaultSettingsProvider.createDefault2DPuck
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import com.mapbox.maps.extension.compose.annotation.generated.CircleAnnotation
 import com.mapbox.maps.extension.compose.annotation.generated.PolylineAnnotation
@@ -39,13 +52,12 @@ import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.animation.camera
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(MapboxExperimental::class)
+    @OptIn(MapboxExperimental::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -61,11 +73,60 @@ class MainActivity : ComponentActivity() {
                     runBlocking(Dispatchers.IO){
                         readFromWebSocket(132){ txt = it }
                     }*/
-                    mapItem()
+
+                    val scaffoldState = rememberBottomSheetScaffoldState()
+
+                    BottomSheetScaffold(
+                        scaffoldState = scaffoldState,
+                        sheetPeekHeight = 158.dp,
+                        sheetContent = { BottomSheetContent(scaffoldState) }
+                    ) {
+                        mapItem()
+                    }
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColumnScope.BottomSheetContent(scaffoldState: BottomSheetScaffoldState, viewModel: MainViewModel = viewModel() ) {
+
+    val state = viewModel.state.value
+    val scope = rememberCoroutineScope()
+
+    LazyRow(
+        Modifier
+            .fillMaxWidth()
+            .background(Color.Red)
+            .height(128.dp)
+    ) {
+        items(state.buses){
+            TextButton(onClick = { viewModel.onEvent(Event.SelectItem(it)) }) {
+                Text(text = it.bus_num.toString(), color = it.color!!)
+            }
+        }
+    }
+    
+    
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Sheet content")
+        Spacer(Modifier.height(20.dp))
+        Button(
+            onClick = {
+                scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+            }
+        ) {
+            Text("Click to collapse sheet")
+        }
+    }
+
 }
 
 @OptIn(MapboxExperimental::class)
